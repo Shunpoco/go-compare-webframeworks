ab7dbb9c4af9a7c9d865868ae5acaf2f4ef191fc
Shunpoco <tkngsnsk313320@gmail.com>
2021-08-01 18:17:03 +0900

    url parse

--- net-http/main.go
+++ net-http/main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+	"net/url"
 )
 
 func main() {
@@ -18,6 +20,12 @@ func main() {
 func ping(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
+
+	url, err := url.ParseQuery(r.URL.RawQuery)
+	if err != nil {
+		log.Println(err)
+	}
+	fmt.Println(url)
 
 	p := make(map[string]string)
 	p["message"] = "pong"
