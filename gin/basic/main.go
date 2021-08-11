1399163cb1def09120deee48c081de78d2437ebe
Shunpoco <tkngsnsk313320@gmail.com>
2021-08-11 12:59:32 +0900

    rewrite

--- gin/basic/main.go
+++ gin/basic/main.go
@@ -14,20 +14,20 @@ func setupRouter() *gin.Engine {
 	r := gin.Default()
 
 	// Ping test
-	r.GET("/ping", ping)
+	r.GET("/ping", pingHandler)
 
 	// Get user value
-	r.GET("/user/:name", user)
+	r.GET("/user/:name", userHandler)
 
 	// Authorized group (users gin.BasicAuth() middleware)
 	// Same than:
 	// authorized := r.Group("/")
 	// authorized.Use(gin.BasicAuth(gin.Credentials{
-	//	"foo": "bar",
-	//	"manu": "123",
+	//	 "foo": "bar",
+	//	 "manu": "123",
 	// }))
 	authorized := r.Group("/", gin.BasicAuth((gin.Accounts{
-		"foo":  "bar", // user: foo, password: bar
+		"foo":  "bar", // user: foo, password: bar,
 		"manu": "123", // user: manu, password: 123
 	})))
 
@@ -41,16 +41,17 @@ func setupRouter() *gin.Engine {
 			-H 'content-type: application/json' \
 			-d '{"value": "bar"}'
 	*/
-	authorized.POST("admin", admin)
+
+	authorized.POST("admin", adminHandler)
 
 	return r
 }
 
-func ping(c *gin.Context) {
+func pingHandler(c *gin.Context) {
 	c.String(http.StatusOK, "pong")
 }
 
-func user(c *gin.Context) {
+func userHandler(c *gin.Context) {
 	user := c.Params.ByName("name")
 	value, ok := db[user]
 	if ok {
@@ -60,7 +61,7 @@ func user(c *gin.Context) {
 	}
 }
 
-func admin(c *gin.Context) {
+func adminHandler(c *gin.Context) {
 	user := c.MustGet(gin.AuthUserKey).(string)
 
 	var json struct {
